b05c4a8479872d95b989ca5ed05c41ef28f3fb28
Andrey Belyaev <ianbel263@gmail.com>
2022-01-10 16:34:56 +0300

    add layout and index vues

---
+++ src/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppIndex />
+</template>
+
+<script>
+import AppIndex from "@/views/Index";
+export default {
+  name: "AppLayout",
+  components: {
+    AppIndex,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/scss/layout/layout";
+</style>
